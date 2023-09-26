032339906286a47451a7ccfbb691c6259480c6bb
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2023-09-26 11:04:08 +0100

    setting up apis

--- Movie app/ContentView.swift
+++ Movie app/ContentView.swift
@@ -8,15 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = MovieDBViewModel()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!20")
+            Text("Hello, world!")
         }
         .padding()
     }
+}
+class MovieDBViewModel: ObservableObject{
+    @Published var homeScreen:[ TrendingItem]
+    
+    func
+}
+
+struct TrendingItem: Identifiable, Decodable{
+    let adult: Bool
+    let id: Int
+    let poster_path: String
+    let title: String
+    let vote_average: Float
 }
 
 struct ContentView_Previews: PreviewProvider {
