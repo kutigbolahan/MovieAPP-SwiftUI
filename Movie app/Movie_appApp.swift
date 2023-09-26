32627d20b44bf911e079f04e6eb6933d12193e96
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2023-09-26 10:27:51 +0100

    Initial Commit

---
+++ Movie app/Movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_appApp.swift
+//  Movie app
+//
+//  Created by GB on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
