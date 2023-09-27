5679132f92ffb882fa39985b4113dc30696a1a22
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2023-09-27 12:25:29 +0100

    Setting up the json model

--- Movie app/ContentView.swift
+++ Movie app/ContentView.swift
@@ -18,12 +18,20 @@ struct ContentView: View {
         }
     }
 }
+@MainActor
 class MovieDBViewModel: ObservableObject{
-    @Published var homeScreen:[ TrendingItem] = []
-    
+    @Published var homeScreen:[TrendingItem] = []
+    static let apiKey = "b5f9b02d0f5d57d2f87ea214b56f63af"
+    static let token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWY5YjAyZDBmNWQ1N2QyZjg3ZWEyMTRiNTZmNjNhZiIsInN1YiI6IjYwY2RmNmUwMGE5NGQ0MDA3OTQ4MDQ2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AkIA0pXmG8FK9IYOgM2gIJwPhISz-papJpv5Y-RuU4E"
+    //https://api.themoviedb.org/3/movie/day?api_key=b5f9b02d0f5d57d2f87ea214b56f63af
     func loadTrending(){
         Task{
-            
+            let url = URL(string: "https://api.themoviedb.org/3/trending/movie/day?api_key=\(MovieDBViewModel.apiKey)")!
+            do{
+                let(data, response) = try await URLSession.shared.data(from: url)
+            }catch{
+                print(error.localizedDescription)
+            }
         }
     }
 }
