452513fb2bc99b52e873650a09165c10ce6ab6de
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2023-09-27 16:49:47 +0100

    first response

--- Movie app/ContentView.swift
+++ Movie app/ContentView.swift
@@ -11,7 +11,18 @@ struct ContentView: View {
     @StateObject var viewModel = MovieDBViewModel()
     var body: some View {
         VStack {
-           
+            if viewModel.trending.isEmpty{
+                Text("No Results")
+            } else{
+                ScrollView(.horizontal){
+                    HStack{
+                        ForEach(viewModel.trending){
+                            trendingItem in
+                            Text(trendingItem.title)
+                        }
+                    }
+                }
+            }
         }
         .padding().onAppear{
             viewModel.loadTrending()
@@ -20,7 +31,7 @@ struct ContentView: View {
 }
 @MainActor
 class MovieDBViewModel: ObservableObject{
-    @Published var homeScreen:[TrendingItem] = []
+    @Published var trending:[TrendingItem] = []
     static let apiKey = "b5f9b02d0f5d57d2f87ea214b56f63af"
     static let token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWY5YjAyZDBmNWQ1N2QyZjg3ZWEyMTRiNTZmNjNhZiIsInN1YiI6IjYwY2RmNmUwMGE5NGQ0MDA3OTQ4MDQ2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AkIA0pXmG8FK9IYOgM2gIJwPhISz-papJpv5Y-RuU4E"
     //https://api.themoviedb.org/3/movie/day?api_key=b5f9b02d0f5d57d2f87ea214b56f63af
@@ -28,12 +39,21 @@ class MovieDBViewModel: ObservableObject{
         Task{
             let url = URL(string: "https://api.themoviedb.org/3/trending/movie/day?api_key=\(MovieDBViewModel.apiKey)")!
             do{
-                let(data, response) = try await URLSession.shared.data(from: url)
+                let(data, _) = try await URLSession.shared.data(from: url)
+                let trendingResults = try JSONDecoder().decode(TrendingResults.self, from: data)
+                trending = trendingResults.results
             }catch{
                 print(error.localizedDescription)
             }
         }
     }
+}
+
+struct TrendingResults: Decodable{
+    let page: Int
+    let results: [TrendingItem]
+    let total_pages: Int
+    let total_results: Int
 }
 
 struct TrendingItem: Identifiable, Decodable{
